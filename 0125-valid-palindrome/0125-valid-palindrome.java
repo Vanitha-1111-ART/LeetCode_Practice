8f1deb0fd094876f6fd0ede52ee358f708802e95
Vanitha kanagaraj <vanitha.kanagaraj2006@gmail.com>
2026-07-24 21:39:24 +0530

    Time: 3 ms (59.19%), Space: 44 MB (96.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,37 @@
 class Solution {
+    boolean isAlphanumeric(char c) {
+        if ((c <= 'Z' && c >= 'A') || (c >= 'a' && c <= 'z') || (c <= '9' && c >= '0')) {
+            return true;
+        }
+        return false;
+    }
+
     public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
         s=s.toLowerCase();
-        s=s.replaceAll("[^a-z0-9]","");
-        s=s.replace(" ","");
 
+        while (i < j) {
 
-        int i=0;
-        int j=s.length()-1;
-        System.out.print(s);
-        while(j>i){
-            if(s.charAt(i)==s.charAt(j)){
+            if (isAlphanumeric(s.charAt(i)) && isAlphanumeric(s.charAt(j))) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    return false;
+                }
                 i++;
                 j--;
+            } else {
+                if (!isAlphanumeric(s.charAt(i))) {
+                    i++;
+                    continue;
+                }
+                if (!isAlphanumeric(s.charAt(j))) {
+                    j--;
+                    ;
+                    continue;
+                }
             }
-            else{
-                return false;
-            }
-            
         }
         return true;
+
     }
 }
