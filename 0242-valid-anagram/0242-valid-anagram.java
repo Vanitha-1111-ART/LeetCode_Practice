8bfb66fddfadf3a49df840ac9f7753e50671226c
Vanitha kanagaraj <vanitha.kanagaraj2006@gmail.com>
2026-07-26 15:47:26 +0530

    Time: 4 ms (82.47%), Space: 44.4 MB (75.02%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+         int[] fre=new int[26];
+         for(int i=0;i<s.length();i++){
+            fre[s.charAt(i)-'a']++;
+         }
+         for(int i=0;i<t.length();i++){
+            fre[t.charAt(i)-'a']--;
+         }
+         for(int i=0;i< s.length();i++){
+            if(fre[s.charAt(i)-'a']!=0) return false;
+         }
+         
+    return true;
+        
+    }
+}
