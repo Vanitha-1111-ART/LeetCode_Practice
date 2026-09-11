48f954e40a4b47cdc4918df4146be37a3759936f
Vanitha kanagaraj <vanitha.kanagaraj2006@gmail.com>
2026-09-11 09:11:55 +0530

    Time: 0 ms (100%), Space: 42 MB (70.06%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public int fib(int n) {
-
-        if (n == 0) {
-            return 0;
+        if(n==0) return 0;
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int t=a+b;
+            a=b;
+            b=t;
         }
-
-        if (n == 1) {
-            return 1;
-        }
-
-        return fib(n - 1) + fib(n - 2);
+        return b;
+        
     }
 }
