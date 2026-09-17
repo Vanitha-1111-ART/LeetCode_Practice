773e8b67a6603c799fdb9bddcbaf4e7fe95b9151
Vanitha kanagaraj <vanitha.kanagaraj2006@gmail.com>
2026-09-17 10:00:41 +0530

    Time: 4 ms (15.52%), Space: 49.5 MB (11.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,21 +1,19 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
+        int c=0,n=isConnected.length;
         boolean[] seen=new boolean[n+1];
         HashMap<Integer,List<Integer>> map=new HashMap<>();
-        List<Integer> list=new ArrayList<>();
-        int c=0;
-        for(int i=1;i<=isConnected.length;i++){
-            map.put(i,new ArrayList<>());
+        for(int i=0;i<isConnected.length;i++){
+            map.put(i+1,new ArrayList<>());
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-               if(isConnected[i][j]==1 && i!=j){
-                     map.get(i+1).add(j+1);
+        for(int i=0;i<isConnected.length;i++){
+            for(int j=0;j<isConnected[0].length;j++){
+                if(isConnected[i][j]==1  && i!=j){
+                    map.get(i+1).add(j+1);
+                }
             }
-            }    
         }
-        for(int i=1;i<n+1;i++){
+       for(int i=1;i<n+1;i++){
             if(!seen[i]){
                 dfs(i,seen,map);
                 c++;
